cc0e45e108775e4b165a7efe173053542af9dbaa
Hanya-Ahmad <hanya.taweel01@eng-st.cu.edu.eg>
2023-12-09 19:06:02 +0200

    [Cli Ophthalmology] removed root package.json

--- client/src/modules/clinics-portals/ophthalmology/pages/index.tsx
+++ client/src/modules/clinics-portals/ophthalmology/pages/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ClinicTitle from '../components/ClinicTitle'; // Import ClinicTitle component
+import React from "react";
+import ClinicTitle from "../components/ClinicTitle/ClinicTitle"; // Import ClinicTitle component
 
 const OphthalmologyClinic = () => {
   return (
     <>
-      <ClinicTitle title="Ophthalmology Clinic" /> {/* Set title for Ophthalmology Clinic */}
+      <ClinicTitle title="Ophthalmology Clinic" />{" "}
+      {/* Set title for Ophthalmology Clinic */}
       {/* Add other components or content specific to the Ophthalmology Clinic */}
     </>
   );
