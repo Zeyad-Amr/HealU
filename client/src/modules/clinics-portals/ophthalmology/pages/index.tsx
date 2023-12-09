e910ee069433aac07a90925357dfa30e3579ffc5
Mariam Mounier <mariam.mostafa1989@gmail.com>
2023-12-09 16:59:28 +0200

    Cli opthalmology

---
+++ client/src/modules/clinics-portals/ophthalmology/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ClinicTitle from '../components/ClinicTitle'; // Import ClinicTitle component
+
+const OphthalmologyClinic = () => {
+  return (
+    <>
+      <ClinicTitle title="Ophthalmology Clinic" /> {/* Set title for Ophthalmology Clinic */}
+      {/* Add other components or content specific to the Ophthalmology Clinic */}
+    </>
+  );
+};
+
+export default OphthalmologyClinic;
