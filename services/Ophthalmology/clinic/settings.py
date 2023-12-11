4d45f114318d490025f9eb6e6e8c73a2fd2010b6
Hanya-Ahmad <hanya.taweel01@eng-st.cu.edu.eg>
2023-12-12 00:05:03 +0200

    [Cli ophthalmology] removed vision test/added .env

--- services/Ophthalmology/clinic/settings.py
+++ services/Ophthalmology/clinic/settings.py
@@ -12,15 +12,23 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c(h0xwpyzxy^_k$n_@p$2dt1@y-=a-g=n208uc^)6*mfrfjn1t'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,9 +86,9 @@ WSGI_APPLICATION = 'clinic.wsgi.application'
 DATABASES = {
     'default': {
          "ENGINE": "django.db.backends.postgresql",
-        "NAME": "billing",
-        "USER": "postgres",
-        "PASSWORD": "root",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": "",
     }
--- services/Ophthalmology/clinic_app/models.py
+++ services/Ophthalmology/clinic_app/models.py
@@ -1,16 +1,1 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-from django.contrib.postgres.fields import ArrayField
-
-class VisionTest(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    appointment_id = models.IntegerField()
-    lt_sph = models.DecimalField()
-    lt_cyl = models.DecimalField()
-    lt_axis = models.IntegerField()
-    rt_sph = models.DecimalField()
-    rt_cyl = models.DecimalField()
-    rt_axis = models.IntegerField()
-    def __str__(self):
-        return f'id {self.id} appt id {self.appointment_id}'
-    
