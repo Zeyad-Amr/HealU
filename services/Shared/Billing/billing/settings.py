12e2f124e14f38fbb1b3e527a557ee6c5a21c87f
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2023-12-11 19:40:47 +0200

    [serv billing] update git ignore + new migration files
    add .pyc to gir ignore , delete old migration and make new migrations file

--- services/Shared/Billing/billing/settings.py
+++ services/Shared/Billing/billing/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "billing",
         "USER": "postgres",
-        "PASSWORD": "root",
+        "PASSWORD": "admin",
         "HOST": "127.0.0.1",
         "PORT": "",
     }
