2c18028c999ca00d22fe6d348f49cd56353a58d8
Ibrahim <imimazim7@gmail.com>
2023-12-12 00:39:30 +0200

    [ Serv Admin ]: Setup admin service application server and database

---
+++ services/Shared/Admin/src/server.ts
@@ -0,0 +1,42 @@
+import app from './app';
+import dotenv from 'dotenv';
+import moongoose from 'mongoose';
+
+// handle uncaught exceptions
+process.on('uncaughtException', (err: Error) => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+// set up environment variables
+dotenv.config({ path: './.env' });
+
+
+const DB = process.env.DATABASE!.replace(
+    '<password>', 
+    process.env.DATABASE_PASSWORD!
+);
+
+moongoose.connect(DB, {}).then(() => {
+    console.log('DB connection successful!');
+});
+
+
+
+const port = process.env.PORT || 3000;
+
+
+const server = app.listen(port, () => {
+    console.log(`Server started & App running on port ${port}`);
+    console.log(`Running on Environment: ${process.env.NODE_ENV}`);
+});
+
+// handle unhandled rejections
+process.on('unhandledRejection', (err: Error) => {
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
