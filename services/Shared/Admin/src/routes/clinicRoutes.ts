b7ba5ee1dcbb0a0df1d865a13c13491cc3431069
Ibrahim <imimazim7@gmail.com>
2023-12-17 23:02:43 +0200

    [ Serv Admin ]: Routing for clinics and clinic services

--- services/Shared/Admin/src/routes/clinicRoutes.ts
+++ services/Shared/Admin/src/routes/clinicRoutes.ts
@@ -7,18 +7,13 @@ const clinicRouter = Router();
 clinicRouter.route("/")
             .get(clinicController.getAllClinics)
             .post(clinicController.createClinic)
-        
-clinicRouter.route("/:id")
+
+clinicRouter.route("/:clinicId")
             .get(clinicController.getClinicByClinicId)
             .patch(clinicController.updateClinic)
             .delete(clinicController.deleteClinic)
 
 
-clinicRouter.route("/doctor/:id")
-                .get(clinicController.getClinicByDoctorId)
-
-clinicRouter.route("/service/:id")
-                .get(clinicController.getClinicByServiceId)
 
 
 export default clinicRouter;
