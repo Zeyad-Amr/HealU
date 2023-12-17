dbe4aca8d8a27905aa433ad91b7b5877a89b9f06
Abram1111 <ebram.meawad01@eng-st.cu.edu.eg>
2023-12-18 00:40:20 +0200

    [ Cli Pediatric ]: Page setup

--- client/src/modules/clinics-portals/pediatric/pages/index.tsx
+++ client/src/modules/clinics-portals/pediatric/pages/index.tsx
@@ -1,53 +1,68 @@
-import React from 'react';
-import ClinicTitle from '../components/title/title';
-import ScheduleTable from '../components/table/table';
-import { useState } from 'react';
+import React from "react";
+import ClinicTitle from "../components/title/title";
+import ScheduleTable from "../components/table/table";
+import { useState } from "react";
+import "./index.css";
 
 const PediatricClinic = () => {
-  const Data  =
-        [
-            {
-                "id": 1,
-                "name": "pedro",
-                "lastName": "Perez",
-                "age": 22,
-                "email": "dummy@gmail.com"
-            },
-            {
-                "id": 2,
-                "name": "abram",
-                "lastName": "gad",
-                "age": 22,
-                "email": "beboabram@gmail.com"
-            },
-            {
-                "id": 3,
-                "name": "naira",
-                "lastName": "youssif",
-                "age": 22,
-                "email": "Nairayoussif@gamil.com"
-            },
-            {
-                "id": 4,
-                "name": "mayar",
-                "lastName": "fayez",
-                "age": 22,
-                "email": "mayarfayez@gamil.com"
-            },
-    ]
+  const Data = [
+    {
+      id: 1,
+      name: "pedro",
+      lastName: "Perez",
+      age: 22,
+      email: "dummy@gmail.com",
+    },
+    {
+      id: 2,
+      name: "abram",
+      lastName: "gad",
+      age: 22,
+      email: "beboabram@gmail.com",
+    },
+    {
+      id: 3,
+      name: "naira",
+      lastName: "youssif",
+      age: 22,
+      email: "Nairayoussif@gamil.com",
+    },
+    {
+      id: 4,
+      name: "mayar",
+      lastName: "fayez",
+      age: 22,
+      email: "mayarfayez@gamil.com",
+    },
+  ];
   const [showData, setShowData] = useState(false);
-  const fetchData = () => { 
+  const fetchData = () => {
     setShowData(true);
   };
 
   return (
     <>
-      <button type="button" onClick={fetchData}>fetch</button>
-      <ClinicTitle />
-      {showData && <ScheduleTable data={Data} />}
-    
+      <div className="clinicContainer">
+        <div className="sideBar">
+          <div className="sideBarContainer">
+            <div className="sideBarTitle">
+              <p>Side Bar</p>
+            </div>
+            <div className="sideBarContent">
+            <button type="button" onClick={fetchData}>
+              fetch
+            </button>
+            </div>
+          </div>
+        </div>
+        <div className="landing">
+          <ClinicTitle />
+          <div className="content">
+            {showData && <ScheduleTable data={Data} />}
+          </div>
+        </div>
+      </div>
     </>
-   
   );
-}
+};
 export default PediatricClinic;
