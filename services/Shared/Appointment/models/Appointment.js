e782661963dd1b0b61a3510dbf12d5b5e6df1190
Dina mostafa <81557785+Dina153@users.noreply.github.com>
2023-12-26 18:35:55 +0200

    Update Appointment.js

--- services/Shared/Appointment/models/Appointment.js
+++ services/Shared/Appointment/models/Appointment.js
@@ -4,30 +4,37 @@ const appointmentSchema = mongoose.Schema({
     slotId: {
         type: String,
         required: [true, "Please enter slot id"],
+          unique: true,
     },
     patientId: {
         type: Number,
         required: [true, "Please enter patient id"],
+          unique: true,
     },
     doctorId: {
         type: Number,
         required: [true, "Please enter doctor id"],
+          unique: true,
     },
     clinicId: {
         type: Number,
         required: [true, "Please enter clinic id"],
+          unique: true,
     },
     status: {
         type: String,
         required: [true, "Please enter the appointment's status"],
+          unique: true,
     },
     date: {
         type: Date,
         required: [true, "Please enter the appointment's date"],
+          unique: true,
     },
     time: {
         type: String,
         required: [true, "Please enter the appointment's time"],
+          unique: true,
     },
 });
 
