97c4cdacbe39e614a34e0f16e6b54c6777e25539
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-12-30 01:41:50 +0300

    Add analytics data retrieval form some servcies

--- services/Shared/Analytics/src/controllers/analyticsController.ts
+++ services/Shared/Analytics/src/controllers/analyticsController.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
 import { errorHandler } from '../services/errorHandler';
+import { getNumberOfClinics, getNumberOfServices } from '../services/adminService';
+import { getNumberOfAppointments } from '../services/appointmentsService';
 const get_analytics = async (req: Request, res: Response) => {
     try {
-        const [allClinicsRes] = await Promise.all([
+        console.log("Analytics service is called")
+        console.log(`${process.env.Admin_URL}/api/v1/clinic`)
+        console.log(`${process.env.Admin_URL}/api/v1/clinicService`)
+        console.log(`${process.env.Appointment_URL}/appointments`)
+
+
+        const [allClinicsRes, allServicesRes, allAppoitments] = await Promise.all([
             axios.get(`${process.env.Admin_URL}/api/v1/clinic`),
+            axios.get(`${process.env.Admin_URL}/api/v1/clinicService`),
+            axios.get(`${process.env.Appointment_URL}/appointments`),
         ])
-        console.log(allClinicsRes.data.data.clinics);
 
         return res.status(200).json({
-            msg: "Hello, World!",
-            clinics: allClinicsRes.data.data.clinics.length
+            msg: "Analytics data returned successfull",
+            clinics: getNumberOfClinics(allClinicsRes.data.data.clinics),
+            services: getNumberOfServices(allServicesRes.data.data.clinicServices),
+            appointments: getNumberOfAppointments(allAppoitments.data),
         })
 
     } catch (error: any) {
