9db2a6d8b6ce19f40479eed2f985a00b8098fa62
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-12-30 02:24:33 +0300

    Add analytics data for doctors, patients, documents, prescriptions, records, and medical history

--- services/Shared/Analytics/src/controllers/analyticsController.ts
+++ services/Shared/Analytics/src/controllers/analyticsController.ts
@@ -3,33 +3,61 @@ import axios from 'axios';
 import { errorHandler } from '../services/errorHandler';
 import { getNumberOfClinics, getNumberOfServices } from '../services/adminService';
 import { getNumberOfAppointments } from '../services/appointmentsService';
+import { getNumberOfDoctors, getNumberOfPatients } from '../services/registrationService';
+import { getNumberOfDocuments } from '../services/storageService';
+import { getNumberOfMedicalHistory, getNumberOfMedicalRecords, getNumberOfPrescriptions } from '../services/emrService';
 const get_analytics = async (req: Request, res: Response) => {
     try {
-        console.log("Analytics service is called")
-        console.log(`${process.env.Admin_URL}/api/v1/clinic`)
-        console.log(`${process.env.Admin_URL}/api/v1/clinicService`)
-        console.log(`${process.env.Appointment_URL}/appointments`)
 
+        console.log("Analytics service is called");
 
-        const [allClinicsRes, allServicesRes, allAppoitments] = await Promise.all([
+        const [
+            allClinicsRes,
+            allServicesRes,
+            allAppoitments,
+            allStaffs,
+            allUsers,
+            allImages,
+            allFiles,
+            allPrescriptions,
+            allReports,
+            allMedicalHistory
+        ] = await Promise.all([
+
             axios.get(`${process.env.Admin_URL}/api/v1/clinic`),
             axios.get(`${process.env.Admin_URL}/api/v1/clinicService`),
             axios.get(`${process.env.Appointment_URL}/appointments`),
-        ])
+            axios.get(`${process.env.Registration_URL}/staff`),
+            axios.get(`${process.env.Registration_URL}/user`),
+            axios.get(`${process.env.Storage_URL}/api/v1/images`),
+            axios.get(`${process.env.Storage_URL}/api/v1/files`),
+            axios.get(`${process.env.EMR_URL}/prescription`),
+            axios.get(`${process.env.EMR_URL}/record`),
+            axios.get(`${process.env.EMR_URL}/medical-history`),
+
+        ]);
 
         return res.status(200).json({
-            msg: "Analytics data returned successfull",
-            clinics: getNumberOfClinics(allClinicsRes.data.data.clinics),
-            services: getNumberOfServices(allServicesRes.data.data.clinicServices),
-            appointments: getNumberOfAppointments(allAppoitments.data),
-        })
+            status: 'success',
+            message: 'Analytics data fetched successfully',
+            data: {
+                clinics: getNumberOfClinics(allClinicsRes.data.data.clinics),
+                services: getNumberOfServices(allServicesRes.data.data.clinicServices),
+                appointments: getNumberOfAppointments(allAppoitments.data),
+                doctors: getNumberOfDoctors(allStaffs.data.data),
+                patients: getNumberOfPatients(allUsers.data.data),
+                documents: getNumberOfDocuments(allImages.data.data.images, allFiles.data.data.files),
+                prescriptions: getNumberOfPrescriptions(allPrescriptions.data),
+                records: getNumberOfMedicalRecords(allReports.data),
+                medicalHistory: getNumberOfMedicalHistory(allMedicalHistory.data),
+            }
+        });
 
     } catch (error: any) {
-        const err = errorHandler(error)
-        res.status(err?.statusCode ?? 500).json(err)
+        const err = errorHandler(error);
+        res.status(err?.statusCode ?? 500).json(err);
     }
 }
-
 
 export {
     get_analytics
