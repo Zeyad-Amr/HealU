66cc75a52bbc5fe5d015d88fddb18c4a7145fae3
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2024-01-02 07:25:40 +0300

    custom axios api & token auth

--- client/src/core/api/api.tsx
+++ client/src/core/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://healu-api-gateway.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
