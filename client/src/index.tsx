6951c3e4bdb90b8af61c828ade5a4ad0567c56ab
Rawda-Yousry <rawda.ahmed02@eng-st.cu.edu.eg>
2024-01-02 21:15:20 +0200

    MergeEdit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,7 +9,6 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
-  document.getElementById("root") as HTMLElement
 );
 root.render(
   // <React.StrictMode>
